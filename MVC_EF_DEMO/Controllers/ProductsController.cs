e8414901258cb678322f723278a71d043af3c31d
Black-Jeff <495090836@qq.com>
2020-05-11 16:40:32 +0800

    提交对应MVC案例

---
+++ MVC_EF_DEMO/Controllers/ProductsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_EF_DEMO.Models;
+
+namespace MVC_EF_DEMO.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private MyConnection db = new MyConnection();
+
+        //// GET: Products
+        //public ActionResult Index()
+        //{
+        //    return View(db.Products.ToList());
+        //}
+
+        // GET: Products
+        public ActionResult Index(string Search_code, string Search_name, string Search_spec)
+        {
+
+
+            if (!string.IsNullOrEmpty(Search_code)
+              || !string.IsNullOrEmpty(Search_name)
+              || !string.IsNullOrEmpty(Search_spec))
+            {
+                var prod = from u in db.Products
+                           where u.model_name.Contains(Search_code)
+                           && u.product_name.Contains(Search_name)
+                           && u.size.Contains(Search_spec)
+                           select u;
+                return View(prod.ToList());
+            }
+            else
+            {
+                var prod = from u in db.Products
+                           //where 1 != 1
+                           select u;
+                return View(prod.ToList());
+            }
+
+
+        }
+        
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductsDB productsDB = db.Products.Find(id);
+            if (productsDB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productsDB);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,price,model_name,product_name,size,counting_unit")] ProductsDB productsDB)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(productsDB);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productsDB);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductsDB productsDB = db.Products.Find(id);
+            if (productsDB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productsDB);
+        }
+
+        // POST: Products/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,price,model_name,product_name,size,counting_unit")] ProductsDB productsDB)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productsDB).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productsDB);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductsDB productsDB = db.Products.Find(id);
+            if (productsDB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productsDB);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductsDB productsDB = db.Products.Find(id);
+            db.Products.Remove(productsDB);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC_EF_DEMO/Models/ProductsDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVC_EF_DEMO.Models
+{
+    public class ProductsDB
+    {
+        public int id { get; set; }
+        public double price { get; set; }
+        public string model_name { get; set; }
+        public string product_name { get; set; }
+        public string size { get; set; }
+        public string counting_unit { get; set; }
+
+    }
+
+    public class MyConnection: DbContext
+   {
+      public DbSet<ProductsDB> Products { set; get; }
+   }
+}
